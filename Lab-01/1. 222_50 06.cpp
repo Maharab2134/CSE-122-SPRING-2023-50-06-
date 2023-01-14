231efa5e38ea679b61598fe76713d32e7b4b6453
Md.Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-14 18:29:25 +0600

    Add files via upload

---
+++ Lab-01/1. 222_50 06.cpp
@@ -0,0 +1,13 @@
+//poblem Number: 4A
+//Povlem Name: Watermelon
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int w=0;
+    cin>>w;
+    string output = (w <=2 || w % 2 ) ? "NO":"YES";
+    cout<< output << endl;
+    return 0;
+}
