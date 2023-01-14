1e789c5286872456a9bf0003e1c9c43ab42c3405
Md.Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-14 18:42:36 +0600

    Add files via upload

---
+++ Lab-02/3. 222_50 06.cpp
@@ -0,0 +1,18 @@
+//poblem Number: 231A
+//Povlem Name: Team
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count = 0, p, v, t, n;
+    cin>>n;
+    while(n){
+        cin>>p>>v>>t;
+        if(p+v+t>=2){
+            count++;
+        }
+        n--;
+    }
+    cout<<count<<endl;
+}
