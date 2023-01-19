abb9b76d3da5eb6e7b7e29b723d5d3b5830decb3
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-19 15:34:38 +0600

    Add files via upload

---
+++ Lab-02/5. 222_50 06.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+ 
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+ 
+    ll m, n, ans=0;
+    cin >> m >> n;
+    ans = (m*n)/2;
+    cout << ans;
+    return 0;
+}
