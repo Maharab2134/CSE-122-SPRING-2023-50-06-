e8b4a355f7b59472da203778684f138ea137e76e
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-01-30 08:52:37 +0600

    Add files via upload

---
+++ Lab-04/7. 222_50 06.cpp
@@ -0,0 +1,19 @@
+//poblem Number: 263A
+//Povlem Name:  Beautiful Matrix
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j,ans=0,n;
+    for(i=1; i<=5; i++)
+        for(j=1; j<=5; j++)
+        {
+            cin>>n;
+            if(n==1)
+                ans=abs(i-3)+abs(j-3);
+        }
+    cout<<ans<<endl;
+
+    return 0;
+}
