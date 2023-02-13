8cc133b9b69676d9801c4920f954041b66a75a2d
Md. Maharab Hosen <122199435+Maharab2134@users.noreply.github.com>
2023-02-13 11:00:02 +0600

    Update 11. 222_50 06.cpp

--- Lab-06/11. 222_50 06.cpp
+++ Lab-06/11. 222_50 06.cpp
@@ -1,44 +1,24 @@
-//poblem Number: 1783A
-//Povlem Name: Make it Beautiful
+//poblem Number: 266A
+//Povlem Name: Stones on the Table
+
 
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    int n;
-    cin>>n;
-    vector<int> arr;
-    for(int i=0; i<n;i++){
-       int val;
-       cin>>val;
-       arr.push_back(val);
-    }
-    sort(arr.begin(), arr.end(), greater<int>());
-    for(int i=0;i<n-1;i++){
-        if(arr[i]==arr[i+1]){
-            swap(arr[i], arr[n-1]);
-        }
-    }
-    int sum=0;   
-    for(int i=0;i<n;i++){
-        if(sum==arr[i]){
-            cout<<"NO"<<"\n";
-            return;
-        }else{
-            sum+=arr[i];
-        }
-    }
-    cout<<"YES"<<"\n";
-    for (auto i : arr)
-        cout << i << " ";
-    cout<<"\n";
-    return;
-}
-
 int main(){
-    int tc;
-    cin>>tc;
-    while(tc--){
-        solve();
-    }
+	int n, count=0;
+	string stones;
+	
+	cin>>n;
+	cin>>stones;
+	
+	for(int i=1; i<n; i++){
+		if(stones[i]==stones[i-1]){
+			stones[i] = stones[i-1];
+			count++;
+		}		
+	}
+	cout<<count;
+	
+	return 0;
 }
